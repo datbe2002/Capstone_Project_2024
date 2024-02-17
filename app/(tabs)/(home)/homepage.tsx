c3b571fb5cc3cc0b29be136111dcc53b630e2764
Kayt <trieuphamkt59@gmail.com>
2024-02-17 13:01:23 +0700

    home section  outline + temp categories

--- app/(tabs)/(home)/homepage.tsx
+++ app/(tabs)/(home)/homepage.tsx
@@ -1,22 +1,143 @@
-import { StyleSheet } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Pressable,
+  StyleSheet,
+} from "react-native";
 
-import { Text, View } from '../../../components/Themed';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { useRouter } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View } from "../../../components/Themed";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import CustomInput from "../../../components/Input";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { COLORS } from "../../../assets";
+import { useState } from "react";
+const { height, width } = Dimensions.get("window");
+
+import { categories } from "../exampledata";
 
 export default function HomepageScreen() {
-  const router = useRouter()
+  const router = useRouter();
+
+  const [searchValue, setSearchValue] = useState<string>("");
+
+  const handleSearch = (text: string) => {
+    setSearchValue(text);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>hello world</Text>
-      <View>
-        <TouchableOpacity onPress={() => router.replace('/introduce')}>
-          <Text>hello homepage</Text>
-        </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      {/* <View>
+          <TouchableOpacity onPress={() => router.replace("/introduce")}>
+            <Text>hello homepage</Text>
+          </TouchableOpacity>
+        </View> */}
+      <View style={[styles.horizWrapper, styles.searchBoxWrapper]}>
+        <Text style={styles.title}>Shop</Text>
+        <View>
+          <CustomInput
+            placeholder="Tìm kiếm..."
+            onChangeText={handleSearch}
+            value={searchValue}
+            style={styles.searchInput}
+            elementAfter={
+              <FontAwesome5 name="search" size={22} color={COLORS.primary} />
+            }
+          />
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={[styles.horizWrapper, styles.cateTitleWrapper]}>
+          <Text style={styles.title}>Categories</Text>
+          <Pressable
+            style={styles.horizWrapper}
+            onPress={() => console.log("more")}
+          >
+            <Text style={styles.secondaryTitle}>See All</Text>
+            <FontAwesome5
+              name="arrow-circle-right"
+              size={20}
+              color={COLORS.primary}
+            />
+          </Pressable>
+        </View>
+
+        <View style={styles.cateList}>
+          {categories?.map((item: any, index) => (
+            <View key={index} style={styles.cateCard}>
+              <Image
+                style={styles.cateImg}
+                source={
+                  item.imgSrc
+                    ? { uri: item.imgSrc }
+                    : require("../../../assets/images/default.png")
+                }
+              />
+              <Text>{item.name}</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  horizWrapper: {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "mon-b",
+  },
+  secondaryTitle: {
+    fontSize: 18,
+    fontFamily: "mon-sb",
+  },
+  searchBoxWrapper: {
+    paddingHorizontal: 10,
+    height: "8%",
+    gap: 30,
+  },
+  searchInput: {
+    height: 40,
+    width: "85%",
+    backgroundColor: COLORS.inputBackgroundColor,
+    borderColor: COLORS.inputBackgroundColor,
+    elevation: 2,
+  },
+  cateTitleWrapper: {
+    gap: 30,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+  cateList: {
+    paddingVertical: 10,
+    minHeight: 250,
+    backgroundColor: "aqua",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    gap: 10,
+  },
+  cateCard: {
+    height: width / 2,
+    width: width / 2.2,
+  },
+  cateImg: {
+    height: 50,
+    width: "auto",
+    objectFit: "contain",
+  },
+});
--- components/Input.tsx
+++ components/Input.tsx
@@ -1,33 +1,53 @@
-import React from 'react';
-import { TextInput, StyleSheet, TextStyle, TextInputProps, Dimensions } from 'react-native';
-import { COLORS, SIZES } from '../assets';
-const { height, width } = Dimensions.get('window')
+import React from "react";
+import {
+  TextInput,
+  StyleSheet,
+  TextStyle,
+  TextInputProps,
+  Dimensions,
+  View,
+} from "react-native";
+import { COLORS, SIZES } from "../assets";
+const { height, width } = Dimensions.get("window");
 
 interface CustomInputProps extends TextInputProps {
-    style?: TextStyle;
+  style?: TextStyle;
+  elementAfter?: any;
 }
 
-const CustomInput: React.FC<CustomInputProps> = ({ style = {}, ...props }) => {
-    return (
-        <TextInput
-            {...props}
-            style={[styles.input, style]}
-        />
-    );
+const CustomInput: React.FC<CustomInputProps> = ({
+  style = {},
+  elementAfter,
+  ...props
+}) => {
+  return (
+    <View style={[styles.inputContainer, style]}>
+      <TextInput {...props} style={[styles.input]} />
+      {elementAfter}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    input: {
-        fontFamily: 'mon',
-        height: 60,
-        width: width / 1.2,
-        borderRadius: 16,
-        borderWidth: 1,
-        paddingHorizontal: 16,
-        backgroundColor: 'red',
-        fontSize: SIZES.large,
-    },
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 60,
+    width: width / 1.2,
+    borderRadius: 16,
+    borderWidth: 1,
+    paddingHorizontal: 16,
+    backgroundColor: "red",
+  },
+  input: {
+    fontFamily: "mon",
+    height: "100%",
+    width: "90%",
+    borderColor: "transparent",
+    fontSize: SIZES.large,
+  },
 });
 
 export default CustomInput;
-
