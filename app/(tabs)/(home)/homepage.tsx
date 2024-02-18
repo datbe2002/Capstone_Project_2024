f22c071cf32b8eed87300b7eeb28026006c5891f
Kayt <trieuphamkt59@gmail.com>
2024-02-18 17:58:10 +0700

    Home done

--- app/(tabs)/(home)/homepage.tsx
+++ app/(tabs)/(home)/homepage.tsx
@@ -4,6 +4,7 @@ import {
   KeyboardAvoidingView,
   Platform,
   Pressable,
+  ScrollView,
   StyleSheet,
 } from "react-native";
 
@@ -13,11 +14,16 @@ import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomInput from "../../../components/Input";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { COLORS } from "../../../assets";
+import { COLORS, SHADOWS, SIZES } from "../../../assets";
 import { useState } from "react";
 const { height, width } = Dimensions.get("window");
 
-import { categories } from "../exampledata";
+import {
+  categories,
+  newItems,
+  recommendations,
+  topProducts,
+} from "../exampledata";
 
 export default function HomepageScreen() {
   const router = useRouter();
@@ -30,11 +36,7 @@ export default function HomepageScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      {/* <View>
-          <TouchableOpacity onPress={() => router.replace("/introduce")}>
-            <Text>hello homepage</Text>
-          </TouchableOpacity>
-        </View> */}
+      {/* search box */}
       <View style={[styles.horizWrapper, styles.searchBoxWrapper]}>
         <Text style={styles.title}>Shop</Text>
         <View>
@@ -49,38 +51,159 @@ export default function HomepageScreen() {
           />
         </View>
       </View>
-      <View style={styles.container}>
-        <View style={[styles.horizWrapper, styles.cateTitleWrapper]}>
-          <Text style={styles.title}>Categories</Text>
-          <Pressable
-            style={styles.horizWrapper}
-            onPress={() => console.log("more")}
-          >
-            <Text style={styles.secondaryTitle}>See All</Text>
-            <FontAwesome5
-              name="arrow-circle-right"
-              size={20}
-              color={COLORS.primary}
-            />
-          </Pressable>
-        </View>
-
-        <View style={styles.cateList}>
-          {categories?.map((item: any, index) => (
-            <View key={index} style={styles.cateCard}>
-              <Image
-                style={styles.cateImg}
-                source={
-                  item.imgSrc
-                    ? { uri: item.imgSrc }
-                    : require("../../../assets/images/default.png")
-                }
+      {/* main */}
+      <ScrollView style={styles.container}>
+        {/* categories */}
+        <View style={styles.categories}>
+          <View style={[styles.horizWrapper, styles.cateTitleWrapper]}>
+            <Text style={styles.title}>Danh mục</Text>
+            <Pressable
+              style={styles.horizWrapper}
+              onPress={() => {
+                console.log("more cate");
+              }}
+            >
+              <Text style={styles.secondaryTitle}>Xem tất cả</Text>
+              <FontAwesome5
+                name="arrow-circle-right"
+                size={20}
+                color={COLORS.primary}
               />
-              <Text>{item.name}</Text>
-            </View>
-          ))}
+            </Pressable>
+          </View>
+
+          <View style={styles.cateList}>
+            {categories?.map((item: any, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  console.log("cate " + index);
+                }}
+              >
+                <View style={[styles.cateCard, SHADOWS.medium]}>
+                  <Image
+                    style={styles.cateImg}
+                    source={
+                      item.imgSrc
+                        ? { uri: item.imgSrc }
+                        : require("../../../assets/images/default.png")
+                    }
+                  />
+                  <Text style={[styles.cateTitle, styles.secondaryTitle]}>
+                    {item.name}
+                  </Text>
+                </View>
+              </Pressable>
+            ))}
+          </View>
         </View>
-      </View>
+        {/* top product */}
+        <View style={styles.topProducts}>
+          <Text style={styles.title}>Bán chạy nhất</Text>
+          <View style={styles.topProductsList}>
+            {topProducts.map((item: any, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  console.log("popular " + index);
+                }}
+              >
+                <View style={[styles.topProductItem, SHADOWS.small]}>
+                  <Image
+                    style={styles.topProductImg}
+                    source={
+                      item.imgSrc
+                        ? { uri: item.imgSrc }
+                        : require("../../../assets/images/default.png")
+                    }
+                  />
+                </View>
+              </Pressable>
+            ))}
+          </View>
+        </View>
+        {/* new items */}
+        <View style={styles.newItems}>
+          <View style={[styles.horizWrapper, styles.cateTitleWrapper]}>
+            <Text style={styles.title}>Sản phẩm mới</Text>
+            <Pressable
+              style={styles.horizWrapper}
+              onPress={() => {
+                console.log("more new item");
+              }}
+            >
+              <Text style={styles.secondaryTitle}>Xem tất cả</Text>
+              <FontAwesome5
+                name="arrow-circle-right"
+                size={20}
+                color={COLORS.primary}
+              />
+            </Pressable>
+          </View>
+
+          <ScrollView style={styles.newItemsList} horizontal={true}>
+            {newItems.map((item: any, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  console.log("new item " + index);
+                }}
+              >
+                <View style={styles.itemCard}>
+                  <View style={[styles.itemImgContainer, SHADOWS.medium]}>
+                    <Image
+                      style={styles.itemImg}
+                      source={
+                        item.imgSrc
+                          ? { uri: item.imgSrc }
+                          : require("../../../assets/images/default.png")
+                      }
+                    />
+                  </View>
+                  <Text
+                    style={[styles.itemDes, { height: "auto" }]}
+                    numberOfLines={1}
+                  >
+                    {item.description}
+                  </Text>
+                  <Text style={styles.itemPrice}>{item.price}đ</Text>
+                </View>
+              </Pressable>
+            ))}
+          </ScrollView>
+        </View>
+        {/* recommendations */}
+        <View style={styles.recommendations}>
+          <Text style={styles.title}>Đề Xuất Cho Bạn</Text>
+          <View style={styles.recommendationsList}>
+            {recommendations.map((item: any, index) => (
+              <Pressable
+                key={index}
+                onPress={() => {
+                  console.log("recommend " + index);
+                }}
+              >
+                <View style={styles.recommendCard}>
+                  <View style={[styles.recommendImgContainer, SHADOWS.medium]}>
+                    <Image
+                      style={styles.recommendImg}
+                      source={
+                        item.imgSrc
+                          ? { uri: item.imgSrc }
+                          : require("../../../assets/images/default.png")
+                      }
+                    />
+                  </View>
+                  <Text style={styles.itemDes} numberOfLines={2}>
+                    {item.description}
+                  </Text>
+                  <Text style={styles.itemPrice}>{item.price}đ</Text>
+                </View>
+              </Pressable>
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -97,11 +220,11 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   title: {
-    fontSize: 20,
+    fontSize: SIZES.xLarge,
     fontFamily: "mon-b",
   },
   secondaryTitle: {
-    fontSize: 18,
+    fontSize: SIZES.medium,
     fontFamily: "mon-sb",
   },
   searchBoxWrapper: {
@@ -116,15 +239,16 @@ const styles = StyleSheet.create({
     borderColor: COLORS.inputBackgroundColor,
     elevation: 2,
   },
+  categories: {
+    paddingVertical: 10,
+    gap: 15,
+  },
   cateTitleWrapper: {
     gap: 30,
     paddingHorizontal: 10,
-    paddingVertical: 10,
   },
   cateList: {
-    paddingVertical: 10,
-    minHeight: 250,
-    backgroundColor: "aqua",
+    paddingBottom: 5,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
@@ -132,12 +256,129 @@ const styles = StyleSheet.create({
     gap: 10,
   },
   cateCard: {
-    height: width / 2,
+    height: width / 2.5,
     width: width / 2.2,
+    borderRadius: 10,
+    position: "relative",
+    backgroundColor: COLORS.inputBackgroundColor,
   },
   cateImg: {
-    height: 50,
-    width: "auto",
-    objectFit: "contain",
+    height: width / 2.6,
+    width: width / 2.2,
+    top: 0,
+    objectFit: "fill",
+    position: "absolute",
+    backgroundColor: "transparent",
+  },
+  cateTitle: {
+    position: "absolute",
+    bottom: 10,
+    left: 10,
+  },
+  topProducts: {
+    minHeight: 120,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    gap: 10,
+  },
+  topProductsList: {
+    paddingVertical: 10,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 5,
+  },
+  topProductItem: {
+    width: width / 5.5,
+    height: width / 5.5,
+    borderRadius: width / 10,
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  topProductImg: {
+    width: width / 6.6,
+    height: width / 6.6,
+    borderRadius: width / 13,
+    position: "absolute",
+  },
+  newItems: {
+    // minHeight: 500,
+    paddingVertical: 10,
+  },
+  newItemsList: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    display: "flex",
+    flexDirection: "row",
+  },
+  itemCard: {
+    width: width / 3,
+    height: width / 2,
+    alignItems: "center",
+    gap: 5,
+    padding: 5,
+  },
+  itemImgContainer: {
+    width: width / 3.5,
+    height: width / 3,
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  itemImg: {
+    width: width / 3.7,
+    height: width / 3.2,
+    borderRadius: 3,
+    // borderWidth: 1,
+    // borderColor: "red",
+  },
+  itemDes: {
+    paddingVertical: 5,
+    height: width / 8,
+    width: "100%",
+    textAlign: "left",
+    fontSize: SIZES.medium,
+    textAlignVertical: "top",
+  },
+  itemPrice: {
+    width: "100%",
+    textAlign: "left",
+    fontFamily: "mon-b",
+    fontSize: SIZES.large,
+  },
+  recommendations: {
+    paddingHorizontal: 10,
+    minHeight: 200,
+  },
+  recommendationsList: {
+    paddingVertical: 10,
+    paddingBottom: 35,
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    gap: 5,
+  },
+  recommendCard: {
+    width: width / 2.15,
+    minHeight: width / 1.5,
+    paddingBottom: 10,
+    gap: 3,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  recommendImgContainer: {
+    width: width / 2.15,
+    height: width / 2,
+    borderRadius: 7,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  recommendImg: {
+    width: width / 2.3,
+    height: width / 2.15,
+    borderRadius: 5,
+    objectFit: "cover",
   },
 });
--- app/(tabs)/exampledata.js
+++ app/(tabs)/exampledata.js
@@ -4,3 +4,39 @@ export const categories = [
   { id: 3, name: "Mũ" },
   { id: 4, name: "Túi" },
 ];
+
+export const topProducts = [
+  { id: 1, name: "Áo" },
+  { id: 2, name: "Quần" },
+  { id: 3, name: "Mũ" },
+  { id: 4, name: "Túi" },
+  { id: 5, name: "Túi 1" },
+];
+
+export const newItems = [
+  {
+    id: 1,
+    name: "Áo",
+    description:
+      "Áo quần đẹp vlz, còn chờ gì nữa mà không mua, mua đi để xài, mua không xài thì pass lại cho người nhà xài",
+    price: 200000,
+  },
+  { id: 2, name: "Quần", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 3, name: "Mũ", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 4, name: "Túi", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 5, name: "Túi 1", description: "Áo quần đẹp vlz", price: 200 },
+];
+
+export const recommendations = [
+  {
+    id: 1,
+    name: "Áo",
+    description:
+      "Áo quần đẹp vlz, còn chờ gì nữa mà không mua, mua đi để xài, mua không xài thì pass lại cho người nhà xài",
+    price: 200000,
+  },
+  { id: 2, name: "Quần", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 3, name: "Mũ", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 4, name: "Túi", description: "Áo quần đẹp vlz", price: 200 },
+  { id: 5, name: "Túi 1", description: "Áo quần đẹp vlz", price: 200 },
+];
