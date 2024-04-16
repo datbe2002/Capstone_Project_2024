a3da96aed43934263504f8027d6a9e2c9a34a09d
Truong Tan Dat <106851700+datbe2002@users.noreply.github.com>
2024-04-16 22:40:33 +0700

    done chat

--- app/context/firebaseConfig.js
+++ app/context/firebaseConfig.js
@@ -4,26 +4,46 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { getFirestore, collection } from "firebase/firestore";
+import { collection, getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCa5Gyv8zbVQcWQDwGrk-C7XZwF84UYHoI",
+//     authDomain: "ftai-firebase-auth.firebaseapp.com",
+//     databaseURL: "https://ftai-firebase-auth-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "ftai-firebase-auth",
+//     storageBucket: "ftai-firebase-auth.appspot.com",
+//     messagingSenderId: "713920214373",
+//     appId: "1:713920214373:web:f9390a985bb9dfe5751d8c"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const auth = initializeAuth(app, {
+//     persistence: getReactNativePersistence(AsyncStorage)
+// })
+
+
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCa5Gyv8zbVQcWQDwGrk-C7XZwF84UYHoI",
-    authDomain: "ftai-firebase-auth.firebaseapp.com",
-    databaseURL: "https://ftai-firebase-auth-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "ftai-firebase-auth",
-    storageBucket: "ftai-firebase-auth.appspot.com",
-    messagingSenderId: "713920214373",
-    appId: "1:713920214373:web:f9390a985bb9dfe5751d8c"
+    apiKey: "AIzaSyAi0XyLbQmHzvxCVaddl1ltBIGzB5FyR9M",
+    authDomain: "fsvton-18ce5.firebaseapp.com",
+    databaseURL: "https://fsvton-18ce5-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "fsvton-18ce5",
+    storageBucket: "fsvton-18ce5.appspot.com",
+    messagingSenderId: "225303706605",
+    appId: "1:225303706605:web:d7e661bca1007c8561c5e4",
+    measurementId: "G-5YEKNTC5BL"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-})
-
 export const db = getFirestore(app)
 
-export const roomRef = collection(db, 'rooms') 
+export const roomRef = collection(db, 'rooms')
--- components/Menu/Chat/MessageList.tsx
+++ components/Menu/Chat/MessageList.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { COLORS } from '../../../assets'
 
-const MessageList = () => {
-    return (
-        <View>
-            <Text>MessageList</Text>
-        </View>
-    )
+export const MessageItem = ({ message, currentUser }: any) => {
+    if (currentUser?.id === message.userId) {
+        return (
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-end', marginBottom: 10 }}>
+                <View style={{ width: '80%' }}>
+                    <View style={{ display: 'flex', alignSelf: 'flex-end', padding: 10, borderRadius: 10, backgroundColor: COLORS.primary }}>
+                        <Text style={{ color: COLORS.white, fontFamily: 'mon-sb', fontSize: 18 }}>
+                            {message?.text}
+                        </Text>
+                    </View>
+                </View>
+            </View>
+        )
+    } else {
+        return (
+            <View style={{ width: '80%', marginBottom: 10 }}>
+                <View style={{ display: 'flex', alignSelf: 'flex-start', padding: 10, borderRadius: 10, backgroundColor: COLORS.white }}>
+                    <Text style={{ color: COLORS.black, fontFamily: 'mon-sb', fontSize: 18 }}>
+                        {message?.text}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
 }
 
-export default MessageList
 
 const styles = StyleSheet.create({})
